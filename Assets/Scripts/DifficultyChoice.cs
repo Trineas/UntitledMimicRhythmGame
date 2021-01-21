0f29540f8a0d1b114de27572a8559f5e003d31df
Danny Sternbeck <danny.sternbeck@gmail.com>
2021-01-21 12:37:18 +0100

    Prince Choreography finished

--- Assets/Scripts/DifficultyChoice.cs
+++ Assets/Scripts/DifficultyChoice.cs
@@ -12,12 +12,11 @@ public class DifficultyChoice : MonoBehaviour
     public static bool buttonsActive;
     public static bool buttonsAreActive;
 
-    /* For Testing Purposes
+    //For Testing Purposes
     private void Start()
     {
         easyMode = true;
     }
-    */
 
     private void Update()
     {
